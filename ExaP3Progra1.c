cf72f2e791ba49ea05c124360c3d9e026cd199a6
Kevin Maquis <kevin.maquis@udla.edu.ec>
2024-01-23 14:33:59 -0500

    Agregamos un menu sencillo donde vamos a dar las opciones de lo solicitado por el usuario, en este caso ingresar y mostrar datos

--- ExaP3Progra1.c
+++ ExaP3Progra1.c
@@ -10,14 +10,34 @@ struct alumno
     float nota;
 };
 
-
-main(){
-
-
-
-
-
-
+int main()
+{
+    int opcion;
+    do
+    {
+        printf("Menu de opciones\n");
+        printf("1-Ingresar informacion\n");
+        printf("2-Mostrar alumnos\n");
+        printf("3-Salir\n");
+        scanf("%d", &opcion);
+        switch (opcion)
+        {
+        case 1:
+            /* code */
+            break;
+        case 2:
+            break;
+        case 3:
+            printf("Ha salido del programa\n");
+            break;
+        default:
+            if ((opcion > 3) || (opcion < 1))
+            {
+                printf("Opcion no valida\n");
+            }
+            break;
+        }
+    } while (opcion != 3);
 
     return 0;
 }
